a80bde27129572e34ced397a9dd993993e30d954
Max Archer <marcher@signpost.com>
2022-05-23 12:41:47 -0400

    create gatsby app

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Open Sked`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
+
+export default config;
